e5619ee9fdc98ae8e62ce734d9a383f0b9b81456
Jacob Green <x7zifle963@gmail.com>
2023-11-19 09:54:14 -0500

    Added tailwindcss to project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.rs", "./templates/**/*.{html,js}", "./assets/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
