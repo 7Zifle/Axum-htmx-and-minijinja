fa49818c9c6e82a80dbb6c6ef2e6a285d5b2e5e0
Jacob Green <x7zifle963@gmail.com>
2023-11-19 09:31:50 -0500

    Updated code to include htmx

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,79 @@
 use std::sync::Arc;
 
-use axum::{
-    extract::{Path, State},
-    response::IntoResponse,
-    routing::get,
-    Router, Server,
-};
-use axum_template::{engine::Engine, Key, RenderHtml};
+use axum::{extract::State, response::IntoResponse, routing::get, Router, Server};
+use axum_template::{engine::Engine, RenderHtml};
 use minijinja::{path_loader, Environment};
 use serde::Serialize;
+use tokio::sync::Mutex;
+use tower_http::services::ServeDir;
 
 type AppEngine = Engine<Environment<'static>>;
 
-#[derive(Debug, Serialize)]
-pub struct Person {
-    name: String,
-}
+//#[derive(Debug, Serialize)]
+//pub struct Person {
+//name: String,
+//}
 
-async fn get_name(
-    // Obtain the engine
-    State(state): State<Arc<AppState>>,
-    // Extract the key
-    Key(key): Key,
-    Path(name): Path<String>,
-) -> impl IntoResponse {
-    let person = Person { name };
+//async fn get_name(
+//// Obtain the engine
+//State(state): State<Arc<AppState>>,
+//// Extract the key
+//Key(key): Key,
+//Path(name): Path<String>,
+//) -> impl IntoResponse {
+//let person = Person { name };
 
-    RenderHtml(key, state.engine.clone(), person)
+//RenderHtml(key, state.engine.clone(), person)
+//}
+
+#[derive(Serialize)]
+struct TestParameters {
+    names: Vec<String>,
 }
 
 async fn get_test(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    RenderHtml("test.html", state.engine.clone(), {})
+    let mut names = state.names.lock().await;
+    names.push(String::from("New test"));
+
+    RenderHtml(
+        "test.html",
+        state.engine.clone(),
+        TestParameters {
+            names: names.clone(),
+        },
+    )
+}
+
+async fn get_index(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    RenderHtml("index.html", state.engine.clone(), {})
+}
+
+async fn get_htmx_resp(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    RenderHtml("htmx-resp.html", state.engine.clone(), {})
 }
 
 // Define your application shared state
 struct AppState {
     engine: AppEngine,
+    names: Mutex<Vec<String>>,
 }
 
 #[tokio::main]
 async fn main() {
     let mut jinja = Environment::new();
-    jinja
-        .add_template("/old/:name", "<h1>Hello Minijinja!</h1><p>{{name}}</p>")
-        .unwrap();
+    let names = vec![String::from("test"), String::from("test2")];
     jinja.set_loader(path_loader("templates/"));
 
     let shared_state = Arc::new(AppState {
         engine: Engine::from(jinja),
+        names: Mutex::new(names),
     });
 
     let app = Router::new()
-        .route("/old/:name", get(get_name))
-        .with_state(shared_state.clone())
         .route("/test", get(get_test))
+        .route("/", get(get_index))
+        .route("/htmx-test", get(get_htmx_resp))
+        .nest_service("/assets", ServeDir::new("assets"))
         .with_state(shared_state.clone());
 
     println!("Starting server on port 8080");
