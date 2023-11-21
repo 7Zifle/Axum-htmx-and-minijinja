f958a2836f7427a24699d1f6b86aa835f1407911
Jacob Green <x7zifle963@gmail.com>
2023-11-21 09:37:50 -0500

    Added edit, delete and basic animations for TODOs
    - TODOs now work off uuid rather than index
    - Basic CSS animations

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use std::sync::Arc;
 
 use axum::{
-    extract::State,
-    response::{Html, IntoResponse},
-    routing::{get, post},
-    Form, Json, Router, Server,
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get, patch, post},
+    Form, Router, Server,
 };
 use axum_template::{engine::Engine, RenderHtml};
 use minijinja::{path_loader, Environment};
 use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 type AppEngine = Engine<Environment<'static>>;
 
@@ -32,8 +34,14 @@ type AppEngine = Engine<Environment<'static>>;
 //}
 
 #[derive(Serialize)]
-struct TestParameters {
-    todos: Vec<String>,
+struct TodosParameters {
+    todos: Vec<Todo>,
+}
+
+#[derive(Serialize, Clone)]
+struct Todo {
+    id: Uuid,
+    name: String,
 }
 
 async fn get_todos(State(state): State<Arc<AppState>>) -> impl IntoResponse {
@@ -41,7 +49,7 @@ async fn get_todos(State(state): State<Arc<AppState>>) -> impl IntoResponse {
     RenderHtml(
         "test.html",
         state.engine.clone(),
-        TestParameters {
+        TodosParameters {
             todos: todos.clone(),
         },
     )
@@ -59,41 +67,69 @@ async fn get_page_home(State(state): State<Arc<AppState>>) -> impl IntoResponse 
     RenderHtml("home-body.html", state.engine.clone(), {})
 }
 
-#[derive(Deserialize)]
-struct Todo {
+async fn get_edit_form(
+    State(state): State<Arc<AppState>>,
+    Path(uuid): Path<Uuid>,
+) -> impl IntoResponse {
+    let todos = state.todos.lock().await.clone();
+    let todo = todos.into_iter().find(|todo| todo.id == uuid);
+    RenderHtml("form.html", state.engine.clone(), todo)
+}
+
+async fn delete_todo(State(state): State<Arc<AppState>>, Path(uuid): Path<Uuid>) -> StatusCode {
+    let mut todos = state.todos.lock().await;
+    todos.retain(|todo| todo.id != uuid);
+    StatusCode::OK
+}
+
+#[derive(Serialize, Deserialize)]
+struct TodoForm {
     description: String,
+}
+
+async fn update_todo(
+    State(state): State<Arc<AppState>>,
+    Path(uuid): Path<String>,
+    Form(payload): Form<TodoForm>,
+) -> impl IntoResponse {
+    let mut todos = state.todos.lock().await;
+
+    match todos.iter_mut().find(|todo| todo.id.to_string() == uuid) {
+        Some(todo) => {
+            todo.name.clear();
+            todo.name.push_str(&payload.description);
+            ([("HX-Trigger", "reloadTodos")], StatusCode::OK)
+        }
+        None => ([("HX-Trigger", "reloadTodos")], StatusCode::BAD_REQUEST),
+    }
 }
 
 async fn post_todo(
     State(state): State<Arc<AppState>>,
-    Form(payload): Form<Todo>,
+    Form(payload): Form<TodoForm>,
 ) -> impl IntoResponse {
     let mut todos = state.todos.lock().await;
-    todos.push(payload.description);
-    RenderHtml(
-        "test.html",
-        state.engine.clone(),
-        TestParameters {
-            todos: todos.clone(),
-        },
-    )
+    todos.push(Todo {
+        id: Uuid::new_v4(),
+        name: payload.description,
+    });
+    [("HX-Trigger", "reloadTodos")]
 }
 
 // Define your application shared state
 struct AppState {
     engine: AppEngine,
-    todos: Mutex<Vec<String>>,
+    todos: Mutex<Vec<Todo>>,
 }
 
 #[tokio::main]
 async fn main() {
     let mut jinja = Environment::new();
-    let todos = vec![String::from("test"), String::from("test2")];
     jinja.set_loader(path_loader("templates/"));
 
     let shared_state = Arc::new(AppState {
         engine: Engine::from(jinja),
-        todos: Mutex::new(todos),
+        todos: Mutex::new(Vec::<Todo>::new()),
     });
 
     let app = Router::new()
@@ -102,6 +138,9 @@ async fn main() {
         .route("/", get(get_index))
         .route("/page/home", get(get_page_home))
         .route("/api/add-todo", post(post_todo))
+        .route("/api/delete/:id", delete(delete_todo))
+        .route("/api/update/:id", patch(update_todo))
+        .route("/api/edit/:id", get(get_edit_form))
         .nest_service("/assets", ServeDir::new("assets"))
         .with_state(shared_state.clone());
 
